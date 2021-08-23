8be266eb60f3d6f6f0393a3c1e92a63b9c62995b
Kanat Yes <yes.kanat@gmail.com>
2021-08-23 17:07:31 +0300

    first commit

---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import COLORS from "./colors";
+
+const Header = (props) => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>{props.title}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    header: {
+        width: "100%",
+        height: 90,
+        paddingTop: 36,
+        backgroundColor: COLORS.black,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    headerTitle: {
+        color: COLORS.background,
+        fontSize: 18
+    }
+});
+
+export default Header;
---
+++ components/colors.js
@@ -0,0 +1,8 @@
+const COLORS = {
+    background: "#f2f5eb",
+    black: "#2f3c4d",
+    red: "#ca402e",
+    light: "#fba676"
+};
+
+export default COLORS;
